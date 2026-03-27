dcce32b29409e1c09f51bd661f6ea7b8982b2e6d
pcp-expama <tales.solidade@floramdf.com>
2026-03-27 09:06:28 -0300

    Update dados_atuais.js

--- dados_atuais.js
+++ dados_atuais.js
@@ -20,8 +20,8 @@ const MOCK_ESTOQUE_LIQUIDO = [
   {
     "Produto": "PAINEL RIPADO",
     "Perfil": "IMPRESSO",
-    "M3_Estoque_Liquido": 52.43422810159993,
-    "Pecas_Estoque_Liquido": 13436.0
+    "M3_Estoque_Liquido": 52.441909101599975,
+    "Pecas_Estoque_Liquido": 13440.0
   },
   {
     "Produto": "PAINEL RIPADO",
