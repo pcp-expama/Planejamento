409126dcc263f91a4acf8a3639152e37b7de49d4
Lucas Oliveira de Souza <lucas.souza@dominio.concrem.net>
2026-04-28 11:01:02 -0300

    Auto-update: 28/04/2026 11:01

--- dados_atuais.js
+++ dados_atuais.js
@@ -68,8 +68,8 @@ const MOCK_ESTOQUE_LIQUIDO = [
     "Espessura": 0.012,
     "Largura": 0.13335,
     "Comprimento": 2.4384,
-    "M3_Estoque_Liquido": 75.3852438312,
-    "Pecas_Estoque_Liquido": 19320.0
+    "M3_Estoque_Liquido": 54.0807186312,
+    "Pecas_Estoque_Liquido": 13860.0
   },
   {
     "Produto": "PAINEL RIPADO",
